ae444b4af0f849fe944ef3abcf384a20d210ab42
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-09-18 17:34:47 +0300

    Shows node info

--- node_info.sh
+++ node_info.sh
@@ -28,7 +28,7 @@ while test $# -gt 0; do
 		echo -e "${C_LGn}Useful URLs${RES}:"
 		echo -e "https://github.com/SecorD0/Minima/blob/main/node_info.sh - script URL"
 		echo -e "         (you can send Pull request with new texts to add a language)"
-		echo -e "https://t.me/letskynode — node Community"
+		echo -e "https://t.me/OnePackage — noderun and tech community"
 		echo
 		return 0 2>/dev/null; exit 0
 		;;
